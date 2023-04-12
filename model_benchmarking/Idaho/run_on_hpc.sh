c10cd99b3b2258dd5d240a92c00b88a74679cb24
charles-ison <isonc@oregonstate.edu>
2023-04-11 18:04:44 -0700

    Revert "Revert "Updating downloaded data directory.""
    This reverts commit c72bdc16d6810e37f2464f72572b066e15c71980.

--- model_benchmarking/Idaho/run_on_hpc.sh
+++ model_benchmarking/Idaho/run_on_hpc.sh
@@ -3,7 +3,5 @@
 #SBATCH --gres=gpu:1         
 #SBATCH --mem=32G   
 #SBATCH -c 16
-#SBATCH -o runIdahoDataOnHPC.out 
-#SBATCH -e runIdahoDataOnHPC.err
 module load cuda/11.8
-python classification.py
+python -u classification.py
