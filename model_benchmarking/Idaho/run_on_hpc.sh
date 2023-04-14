4b79d4df32ad4b40943faf2539665e58bf558dfb
charles-ison <isonc@oregonstate.edu>
2023-04-14 01:15:35 -0700

    Removing a40 constraint.

--- model_benchmarking/Idaho/run_on_hpc.sh
+++ model_benchmarking/Idaho/run_on_hpc.sh
@@ -4,8 +4,7 @@
 #SBATCH -p share
 #SBATCH -t 4-00:00:00   
 #SBATCH --gres=gpu:1         
-#SBATCH --mem=64G   
+#SBATCH --mem=128G  
 #SBATCH -c 16
-#SBATCH --constraint=a40
 module load cuda/11.8
 python -u classification.py
