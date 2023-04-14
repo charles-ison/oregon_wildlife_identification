88f8b7249619bdb26e079afddecd8e0ac394a179
charles-ison <isonc@oregonstate.edu>
2023-04-14 01:22:45 -0700

    Changing server type.

--- model_benchmarking/Idaho/run_on_hpc.sh
+++ model_benchmarking/Idaho/run_on_hpc.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -J runIdahoDataOnHPC
 #SBATCH -A eecs 
-#SBATCH -p share
+#SBATCH -p gpu
 #SBATCH -t 4-00:00:00   
 #SBATCH --gres=gpu:1         
 #SBATCH --mem=128G  
 #SBATCH -c 16
+#SBATCH --constraint=rtx8000
 module load cuda/11.8
 python -u classification.py
