eaff2287be2d207f45cdd2dfa4a5cebce01ca0bf
charles-ison <isonc@oregonstate.edu>
2023-11-28 14:38:12 -0800

    Adding preprocessing for caltech datetimes.

--- model_benchmarking/preprocessing_scripts/caltech_preprocessing.py
+++ model_benchmarking/preprocessing_scripts/caltech_preprocessing.py
@@ -1,6 +1,7 @@
 import json
 import os
 from PIL import Image
+from datetime import datetime
 
 file_name = "animal_count_key.json"
 json_file = open(file_name)
@@ -10,9 +11,12 @@ images = coco_key["images"]
 
 for json_image in images:
     json_image["file_name"] = "images/" + json_image["file_name"]
+    time_stamp = datetime.strptime(json_image["date_captured"], '%Y-%m-%d %H:%M:%S')
+    date_time_string = datetime.strftime(time_stamp, "%Y:%m:%d %H:%M:%S")
+    json_image["datetime"] = date_time_string
 
-#for json_annotation in annotations:
-#    json_annotation["category_id"] = 1 if json_annotation["category_id"] == 34 else 0
+for json_annotation in annotations:
+    json_annotation["category_id"] = 1 if json_annotation["category_id"] == 34 else 0
 
 json_file.close()
 json_file = open(file_name, "w")
