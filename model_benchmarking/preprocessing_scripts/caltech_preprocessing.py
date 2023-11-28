11b6201633a3dc3ca152fce17daec3207fdbdbb4
charles-ison <isonc@oregonstate.edu>
2023-11-28 13:48:26 -0800

    Adding preprocessing to caltech file paths.

--- model_benchmarking/preprocessing_scripts/caltech_preprocessing.py
+++ model_benchmarking/preprocessing_scripts/caltech_preprocessing.py
@@ -6,9 +6,13 @@ file_name = "animal_count_key.json"
 json_file = open(file_name)
 coco_key = json.load(json_file)
 annotations = coco_key["annotations"]
+images = coco_key["images"]
 
-for json_annotation in annotations:
-    json_annotation["category_id"] = 1 if json_annotation["category_id"] == 34 else 0
+for json_image in images:
+    json_image["file_name"] = "images/" + json_image["file_name"]
+
+#for json_annotation in annotations:
+#    json_annotation["category_id"] = 1 if json_annotation["category_id"] == 34 else 0
 
 json_file.close()
 json_file = open(file_name, "w")
