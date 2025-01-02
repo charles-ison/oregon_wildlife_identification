8a73b555892f36e66b0ae3eb2cfe9b80d7b3c4c6
Charles Ison <isonc@submit-b.ib.coehpc>
2025-01-02 15:00:56 -0800

    Saving run time comparison results per GPU.

--- model_benchmarking/batch_animal_count_training_time.py
+++ model_benchmarking/batch_animal_count_training_time.py
@@ -60,6 +60,8 @@ def fetch_training_data(data_dir):
             batch_training_labels.extend(temp_batch_train_lab)
             batch_validation_data.extend(temp_batch_val_data)
             batch_validation_labels.extend(temp_batch_val_lab)
+            
+        break
     
     training_data = training_data[0:int(len(training_data)/10)]
     training_labels = training_labels[0:int(len(training_labels)/10)]
@@ -172,87 +174,22 @@ def validation(model, validation_data_set, mse_criterion, mae_criterion, rank, b
     mae = running_mae/len(validation_data_set)
     accuracy = num_correct/len(validation_data_set)
     return mse, mae, accuracy
-
-
-def batch_validation(model, batch_validation_data_set, mse_criterion, mae_criterion, rank):
-    model.eval()
-    running_mse = 0.0
-    running_mae = 0.0
-    num_correct = 0
-    all_labels, all_predictions = [], []
-
-    for batch in batch_validation_data_set:
-        data, targets = batch['data'], batch['label']
-        labels = utilities.get_labels_from_targets(targets)
-        labels = torch.FloatTensor(labels).to(rank)
-        
-        # This is to prevent cuda memory issues for large batches
-        max_prediction = 0
-        for image in data:
-            image = torch.unsqueeze(image, dim=0).to(rank)
-            output = model(image).flatten()
-            max_prediction = max(max_prediction, output.item())
-
-        max_prediction = torch.tensor(max_prediction).to(rank)
-        max_label = torch.max(labels)
-        
-        running_mse += mse_criterion(max_prediction, max_label).item()
-        running_mae += mae_criterion(max_prediction, max_label).item()
-        if max_prediction.round().item() == max_label:
-            num_correct += 1
-
-        all_predictions.append(max_prediction.item())
-        all_labels.append(max_label.item())
-
-    mse = running_mse/len(batch_validation_data_set)
-    mae = running_mae/len(batch_validation_data_set)
-    accuracy = num_correct/len(batch_validation_data_set)
-    return mse, mae, accuracy, all_labels, all_predictions
     
     
-def batch_validation_object_detection(model, batch_validation_data_set, mse_criterion, mae_criterion, print_incorrect_images, saving_dir, rank):
-    model.eval()
-    running_mse = 0.0
-    running_mae = 0.0
-    num_correct = 0
-    all_labels, all_predictions = [], []
-    count = 0
+def get_thread_subset_of_data(rank, world_size, data, labels):
+    start_index = int((rank/world_size) * len(data))
+    end_index = int(((rank+1.0)/world_size) * len(data))
+    return data[start_index:end_index], labels[start_index:end_index]
 
-    for batch in batch_validation_data_set:
-        data, targets = batch['data'], batch['label']
-        labels = utilities.get_labels_from_targets(targets)
 
-        # This is to prevent cuda memory issues for large batches
-        max_prediction = 0
-        max_label = 0
-        for index, image in enumerate(data):
-            image = torch.unsqueeze(image, dim=0).to(rank)
-            bounding_boxes = model(image)
-            prediction = get_predictions(bounding_boxes)[0]
-            label = labels[index]
-            
-            if print_incorrect_images and label != prediction:
-                utilities.print_image(torch.squeeze(image), prediction, saving_dir + "incorrect_images/", count, bounding_boxes)
-            
-            max_prediction = max(max_prediction, prediction)
-            max_label = max(max_label, label)
-            count += 1
-            
-        running_mse += mse_criterion(torch.FloatTensor([max_label]), torch.FloatTensor([max_prediction])).item()
-        running_mae += mae_criterion(torch.FloatTensor([max_label]), torch.FloatTensor([max_prediction])).item()
-        if max_prediction == max_label:
-            num_correct += 1
+def train_and_validate(rank, world_size, num_epochs, model, model_name, training_data, training_labels, validation_data, validation_labels, saving_dir, batch_size, is_object_detection):
+    
+    training_data, training_labels = get_thread_subset_of_data(rank, world_size, training_data, training_labels)
+    validation_data, validation_labels = get_thread_subset_of_data(rank, world_size, validation_data, validation_labels)
+    
+    training_data_set = ImageDataSet(training_data, training_labels)
+    validation_data_set = ImageDataSet(validation_data, validation_labels)
 
-        all_predictions.append(max_prediction)
-        all_labels.append(max_label)
-
-    mse = running_mse/len(batch_validation_data_set)
-    mae = running_mae/len(batch_validation_data_set)
-    accuracy = num_correct/len(batch_validation_data_set)
-    return mse, mae, accuracy, all_labels, all_predictions
-
-def train_and_validate(rank, world_size, num_epochs, model, model_name, training_data_set, validation_data_set, batch_training_data_set, batch_validation_data_set, saving_dir, batch_size, is_object_detection):
-   
     dist.init_process_group(world_size=world_size, rank=rank)
     torch.cuda.set_device(rank)
     model = model.to(rank)
@@ -268,7 +205,6 @@ def train_and_validate(rank, world_size, num_epochs, model, model_name, training
     for epoch in range(num_epochs):
         training_data_set.shuffle()
         validation_data_set.shuffle()
-        batch_training_data_set.shuffle()
 
         #TODO: Use OOP here
         if is_object_detection:
@@ -277,18 +213,11 @@ def train_and_validate(rank, world_size, num_epochs, model, model_name, training
            training_loss = train(model, training_data_set, huber_loss, optimizer, rank, batch_size)
         #print("training loss: " + str(training_loss))
 
-
         if is_object_detection:
             val_mse, val_mae, val_acc = validation_object_detection(model, validation_data_set, mse, mae, rank, batch_size)
         else:
             val_mse, val_mae, val_acc = validation(model, validation_data_set, mse, mae, rank, batch_size)
         # print("validation MSE: " + str(val_mse) + ", MAE: " + str(val_mae) + " and ACC: " + str(val_acc))
-        
-        if is_object_detection:
-            batch_val_mse, batch_val_mae, batch_val_acc, batch_labels, batch_predictions = batch_validation_object_detection(model, batch_validation_data_set, mse, mae, False, saving_dir, rank)
-        else:
-            batch_val_mse, batch_val_mae, batch_val_acc, batch_labels, batch_predictions = batch_validation(model, batch_validation_data_set, mse, mae, rank)
-        # print("batch validation MSE: " + str(batch_val_mse) + ", MAE: " + str(batch_val_mae) + " and ACC: " + str(batch_val_acc))
         
         print(time.time() - start_time)
         
@@ -314,10 +243,8 @@ if __name__ == '__main__':
     os.environ['MASTER_PORT'] = '12355'
 
     training_data, training_labels, validation_data, validation_labels, batch_training_data, batch_training_labels, batch_validation_data, batch_validation_labels = fetch_training_data(data_dir)
-    training_data_set = ImageDataSet(torch.stack(training_data, dim=0).clone(), training_labels)
-    validation_data_set = ImageDataSet(torch.stack(validation_data, dim=0).clone(), validation_labels)
-    batch_training_data_set = ImageDataSet(batch_validation_data, batch_validation_labels)
-    batch_validation_data_set = ImageDataSet(batch_validation_data, batch_validation_labels)
+    training_data = torch.stack(training_data, dim=0).clone()
+    validation_data = torch.stack(validation_data, dim=0).clone()
 
     for i in range(torch.cuda.device_count()):
 
@@ -335,22 +262,22 @@ if __name__ == '__main__':
         resnet152.fc = nn.Linear(in_features, 1)
 
         retina_net = models.detection.retinanet_resnet50_fpn_v2(weights=models.detection.RetinaNet_ResNet50_FPN_V2_Weights.DEFAULT)
-    
+        
         world_size = i + 1
         print("\nUsing " + str(world_size) + " GPUs")
 
         # Training
         print("\nTraining and Validating ResNet34")
-        mp.spawn(train_and_validate, args=(world_size, num_epochs, resnet34, "ResNet34", training_data_set, validation_data_set, batch_training_data_set, batch_validation_data_set, saving_dir, batch_size, False), nprocs=world_size, join=True)
+        mp.spawn(train_and_validate, args=(world_size, num_epochs, resnet34, "ResNet34", training_data, training_labels, validation_data, validation_labels, saving_dir, batch_size, False), nprocs=world_size, join=True)
 
-        #print("\nTraining and Validating ResNet50")
-        #train_and_validate(num_epochs, resnet50, "ResNet50", training_data_set, validation_data_set, batch_training_data_set, batch_validation_data_set, device, saving_dir, batch_size, False)
+        print("\nTraining and Validating ResNet50")
+        mp.spawn(train_and_validate, args=(world_size, num_epochs, resnet50, "ResNet50", training_data, training_labels, validation_data, validation_labels, saving_dir, batch_size, False), nprocs=world_size, join=True)
 
-        #print("\nTraining and Validating ResNet152")
-        #train_and_validate(num_epochs, resnet152, "ResNet152", training_data_set, validation_data_set, batch_training_data_set, batch_validation_data_set, device, saving_dir, batch_size, False)
+        print("\nTraining and Validating ResNet152")
+        mp.spawn(train_and_validate, args=(world_size, num_epochs, resnet152, "ResNet152", training_data, training_labels, validation_data, validation_labels, saving_dir, batch_size, False), nprocs=world_size, join=True)
 
-        #print("\nTraining and Validating RetinaNet")
-        #mp.spawn(train_and_validate, args=(world_size, num_epochs, retina_net, "RetinaNet", training_data_set, validation_data_set, batch_training_data_set, batch_validation_data_set, saving_dir, object_detection_batch_size, True), nprocs=world_size, join=True)
+        print("\nTraining and Validating RetinaNet")
+        mp.spawn(train_and_validate, args=(world_size, num_epochs, retina_net, "RetinaNet", training_data, training_labels, validation_data, validation_labels, saving_dir, object_detection_batch_size, True), nprocs=world_size, join=True)
 
 
 
